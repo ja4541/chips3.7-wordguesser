2875273c0c55cc0924ff21382e9a52cdeaf16fa5
Joe Aronov <ja4541@nyu.edu>
2025-09-29 23:10:05 +0000

    Implement WordGuesserGame core logic

--- lib/wordguesser_game.rb
+++ lib/wordguesser_game.rb
@@ -4,9 +4,41 @@ class WordGuesserGame
 
   # Get a word from remote "random word" service
 
-  def initialize(word)
-    @word = word
+  attr_accessor :word, :guesses, :wrong_guesses
+
+  def initialize(new_word)
+    @word = new_word
+    @guesses = ''
+    @wrong_guesses = ''
   end
+
+  def guess(letter)
+    raise ArgumentError if letter.nil? || letter.empty? || letter !~ /^[A-Za-z]$/
+  
+    letter.downcase!
+    return false if @guesses.include?(letter) || @wrong_guesses.include?(letter)
+  
+    if @word.include?(letter)
+      @guesses << letter
+    else
+      @wrong_guesses << letter
+    end
+    true
+  end
+
+  def word_with_guesses
+    result = ""
+    @word.each_char do |c|
+      result << (@guesses.include?(c) ? c : '-')
+    end
+    result
+  end
+
+  def check_win_or_lose
+    return :win if word_with_guesses == @word
+    return :lose if @wrong_guesses.length >= 7
+    :play
+  end  
 
   # You can test it by installing irb via $ gem install irb
   # and then running $ irb -I. -r app.rb
